1a29737ec1beb14a253f96e7e10ef3f3590a01b2
Dzmitry Piatrovich <piatrovich@live.ru>
2014-09-19 13:50:06 +0300

    5 final solution

---
+++ 5 task/cross_map_final.sql
@@ -0,0 +1,35 @@
+COLUMN GROUP_A FORMAT A80
+COLUMN GROUP_B FORMAT A80
+
+WITH all_authors AS
+(
+SELECT ROWNUM r, AUTHOR_ID, AUTHOR_NAME 
+  FROM (
+    SELECT AUTHOR_ID, AUTHOR_NAME
+    FROM AUTHOR
+    ORDER BY SYS.DBMS_RANDOM.RANDOM
+  )
+), bearing as (
+  SELECT MAX(bearing_element)
+      FROM
+      (
+        SELECT all_authors.r as bearing_element
+        FROM all_authors
+        WHERE ROWNUM <= (
+          SELECT COUNT(*)/2 
+          FROM all_authors
+          )
+      )
+)
+SELECT left_col.AUTHOR_NAME as GROUP_A, right_col.AUTHOR_NAME as GROUP_B
+FROM (
+  SELECT ROWNUM r, AUTHOR_NAME
+  FROM all_authors
+  WHERE all_authors.r <= (SELECT * FROM bearing)
+) left_col,
+(
+  SELECT ROWNUM r, AUTHOR_NAME
+  FROM all_authors
+  WHERE all_authors.r > (SELECT * FROM bearing)
+) right_col
+WHERE left_col.r = right_col.r;
