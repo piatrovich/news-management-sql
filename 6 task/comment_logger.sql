d29d4a1505ecff9c9b21d906d3daa6c0098772da
Dzmitry Piatrovich <piatrovich@live.ru>
2014-10-22 22:55:45 +0300

    added comment logger

--- 6 task/comment_logger.sql
+++ 6 task/comment_logger.sql
@@ -3,15 +3,20 @@ BEFORE INSERT ON COMMENTS FOR EACH ROW
 DECLARE 
   var_description VARCHAR2(4000); 
 BEGIN
-  IF :NEW.TEXT != NULL THEN
-    IF LENGTH(:NEW.TEXT) > 0 THEN
-      var_description := '"TEXT" : "' || :NEW.TEXT || '"';
-    END IF;
+
+  IF LENGTH(:NEW.TEXT) > 0 THEN
+    var_description := '"TEXT" : "' || :NEW.TEXT || '"';
   END IF;
+  
   IF :NEW.CREATION_DATE != NULL THEN
+    IF NVL(LENGTH(var_description), 0) > 0 THEN
+      var_description := var_description || ';';    
+    END IF;
     var_description := '; "CREATION_DATE" : "' || :NEW.CREATION_DATE || '"';
   END IF;
+  
   var_description := '; "NEWS_ID" : "' || :NEW.NEWS_NEWS_ID || '"';
+  
   INSERT INTO LOGGING (INSERT_TIME, TABLE_NAME, TEXT)
   VALUES(SYSDATE, 'TAG', var_description);
 END;
