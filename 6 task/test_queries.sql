ea68141f9d5820269ee7507625011b04fba87af2
Dzmitry Piatrovich <piatrovich@live.ru>
2014-10-22 22:56:51 +0300

    added test queries for loggers

---
+++ 6 task/test_queries.sql
@@ -0,0 +1,21 @@
+-- Testing tag logger
+INSERT INTO TAG (TAG_NAME) 
+VALUES ('John Galt');
+
+-- Testing tag logger
+INSERT INTO AUTHOR (AUTHOR_NAME) 
+VALUES ('Oliver Gierke');
+
+-- Testing comment logger
+INSERT INTO COMMENTS (TEXT, CREATION_DATE, NEWS_NEWS_ID) 
+VALUES ('Who is John Galt?', SYSDATE, 290);
+
+-- Testing news logger
+INSERT INTO NEWS (FULL_TEXT, CREATION_DATE, TITLE) 
+VALUES ('Full news text', SYSDATE, 'News title');
+
+INSERT INTO NEWS (SHORT_TEXT, FULL_TEXT, CREATION_DATE, MODIFICATION_DATE, TITLE) 
+VALUES ('', 'Full news text 2', SYSDATE, SYSDATE, 'News title 2');
+
+INSERT INTO NEWS (SHORT_TEXT, FULL_TEXT, CREATION_DATE, TITLE) 
+VALUES ('Short news text', 'Full news text 3', SYSDATE, 'News title 3');
