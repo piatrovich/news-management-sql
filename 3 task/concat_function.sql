092b0e322bcc8d7c3c4b448c064981e87caea80a
Dzmitry Piatrovich <piatrovich@live.ru>
2014-10-22 22:16:13 +0300

    enhanced own concat function

--- 3 task/concat_function.sql
+++ 3 task/concat_function.sql
@@ -10,26 +10,14 @@ BEGIN
       JOIN TAG ON NEWS_TAG.TAG = TAG.TAG_ID
       WHERE NEWS_TAG.NEWS = NEWS_ID
    )
-      LOOP
-        IF tags IS NOT NULL
-        THEN 
-          IF LENGTH(tags) + LENGTH(tag_set.tag) + LENGTH(SEPARATOR) + 3 <= 4000
+      LOOP 
+          IF NVL(LENGTH(tags), 0) + LENGTH(tag_set.tag) + LENGTH(SEPARATOR) + 3 <= 50
           THEN
             tags := tags || tag_set.tag || SEPARATOR;
           ELSE
             tags := tags || '...';
-            GOTO overflowed;
+            RETURN tags;
           END IF;
-        ELSE
-          IF LENGTH(tag_set.tag) + LENGTH(SEPARATOR) + 3 <= 4000
-          THEN
-            tags := tags || tag_set.tag || SEPARATOR;
-          ELSE
-            tags := tags || '...';
-            GOTO overflowed;
-          END IF;
-        END IF;
       END LOOP; 
-      <<overflowed>> NULL;
   RETURN tags;
 END CONCAT_TAGS;
